4e29d1c9c83ac6010e517e03dd7a3a8894016b93
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2025-08-05 20:24:17 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+console.log("Mary Stuart (1542)");
+console.log("Mary");
+console.log("Stuart");
+console.log("(1542)");
+console.log("Test!");
+console.log("Marry " + "Stuart "+"(1542)");
