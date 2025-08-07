381778c131e1f187a5ef6af1ea588d4c12e4f291
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2025-08-07 22:35:09 +0800

    Update 2.0.14_Section-Quiz_Question-1.js

--- 2.0.14_Section-Quiz_Question-1.js
+++ 2.0.14_Section-Quiz_Question-1.js
@@ -17,16 +17,16 @@ console.log("Total:", x + y + z);
 /* 
 Question:
 Question 1: Let's play florist. Declare six variables, remembering to name them according to their purpose:
-the price of a single rose (8) and the number of roses you have (70)
-the price of a single lily (10) and the number of lilies you have (50)
-the price of a single tulip (2) and the number of tulips you have (120)
+*the price of a single rose (8) and the number of roses you have (70)
+*the price of a single lily (10) and the number of lilies you have (50)
+*the price of a single tulip (2) and the number of tulips you have (120)
 Now declare three variables, one each for the roses, lilies, and tulips you have, 
 in which you place their total price. Insert the corresponding values into the variables 
 using the variables declared in the previous step. Finally, declare a variable in which 
 you store the price of all your flowers (again, use the previous variables for initialization). 
 Display all inventory information in the console in the following form:
 
-Correct Answer which is more readable, maintainable, and scalable.
+*Correct Answer which is more readable, maintainable, and scalable.*
 let rosePrice = 8;
 let lilyPrice = 10;
 let tulipPrice = 2;
