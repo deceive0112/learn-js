4f62a74e0f4c691c07859a97e67ea71d3903de92
deceive0112 <94655133+deceive0112@users.noreply.github.com>
2025-08-10 01:01:53 +0800

    push

---
+++ 2.0.15_Lab-Variables.js
@@ -0,0 +1,21 @@
+let name1 = "Maxwel lWright";
+let name2 = "Raja Villarreal";
+let name3 = "Helen Richards";
+
+let name1Phone = "(0191) 719 6495";
+let name2Phone = "0866 398 2895";
+let name3Phone = "0800 1111";
+
+let name1Email = "Curabitur.egestas.nunc@nonummyac.co.uk";
+let name2Email = "posuere.vulputate@sed.com";
+let name3Email = "libbero@convallis.edu";
+
+console.log(name1, "/", name1Phone, "/", name1Email);
+console.log(name3, "/", name3Phone, "/", name3Email);
+
+let path = "C:\\" + "Windows";
+console.log(path); // -> C:\Windows
+   
+let test = "100" + "10";
+console.log(test); // -> 10010
+console.log(typeof test); // -> string
